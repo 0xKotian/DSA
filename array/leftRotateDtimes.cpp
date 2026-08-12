5051697f4e53cc3d79c15009e985aa3d1a4f44e4
pramukh <kotianpramukh@gmail.com>
2026-08-12 13:21:30 +0530

    left rotate the array by D times

---
+++ array/leftRotateDtimes.cpp
@@ -0,0 +1,26 @@
+/*
+left rotating of the array d times is done by first rotating the array till the dth element and then rotating the elements after dth element and then finally 
+rotating all the elements  
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int a[], int start, int end){
+    while(start <= end){
+        swap(a[start],a[end]);
+        start++;
+        end--;
+    }
+}
+void leftrotate(int a[], int n, int d){
+    reverse(a,0,d-1);
+    reverse(a,d,n-1);
+    reverse(a,0,n-1);
+}
+int main(){
+    int n=7;
+    int a[n] = {1,2,3,4,5,6,7};
+    int d=3;
+    leftrotate(a,n,d);
+    for(int i = 0; i < n; i++) cout<<a[i]<<" ";
+}
