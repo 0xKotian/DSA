617ee7a8c4ddf6eaa2e2cf5d6d4d9419664f84a5
pramukh <kotianpramukh@gmail.com>
2026-08-25 19:17:03 +0530

    find and delete middle node

---
+++ linked list/deleteMiddleNode.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public: 
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+node* gethead(vector<int> &a, int n){
+    node* head = new node(a[0]);
+    node* temp = head;
+    for(int i = 1; i < n; i++){
+        temp->next = new node(a[i]);
+        temp = temp->next;
+    } 
+    return head;
+}
+
+node* DeleteMiddleNode(node* head){
+    if(head == nullptr || head->next == nullptr) return NULL;
+    node* slow = head;
+    node* fast = head;
+    fast = fast->next->next;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    } 
+    node* middle = slow->next;
+    slow->next = slow->next->next;
+    middle->next = nullptr;
+    delete middle;
+    return head;
+}
+
+void FindMiddleNode(node* head){
+    if(head == nullptr){
+        cout<<"empty LL";
+        return;
+    }
+    if(head->next == nullptr){
+        cout<<head->data;
+        return;
+    } 
+    node* slow = head;
+    node* fast = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    } 
+    cout<<"middle ele is "<<slow->data<<endl;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    vector<int> a = {12,8,5,9,10};
+    int n = a.size();
+    node* head = gethead(a,n);
+    FindMiddleNode(head);
+    head = DeleteMiddleNode(head);
+    printLL(head);
+}
