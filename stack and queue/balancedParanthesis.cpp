bba0632adf91627fb0a3ec2c82d06cca915e0a9e
pramukh <kotianpramukh@gmail.com>
2026-09-02 20:46:00 +0530

    next greater element and NSG for circular arr

--- stack and queue/balancedParanthesis.cpp
+++ stack and queue/balancedParanthesis.cpp
@@ -1,3 +1,10 @@
+/*
+expression will be given having paranthesis. return if it is balanced or not. (){[]} -> balanced (){[])} -> not balanced. every opening of paranthesis
+should have its closed paranthesis and vice versa.
+
+logic -> iterate through the string and if its a open paranthesis then push it in stack if its closing paranthesis then its open paranthesis should be
+at the top of stack. when u find the open paranthesis of the closed paranthesis the pop it out. at end check if the stack is empty or not if empty then balanced. 
+*/
 #include<bits/stdc++.h>
 using namespace std; 
 
--- stack and queue/nextGreaterEle.cpp
+++ stack and queue/nextGreaterEle.cpp
@@ -1,3 +1,6 @@
+/*
+u have to find the next greater element of the ith index ele in the given arr.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,12 +8,12 @@ void nextGreaterEle(int a[], int n){
     vector<int> ans;
     stack<int> st;
     for(int i = n-1; i >= 0; i--){
-        while(!st.empty() && st.top() <= a[i]) st.pop();
-        
-        if(st.empty()) ans.push_back(-1);
-        else ans.push_back(st.top());
+        while(!st.empty() && st.top() <= a[i]) st.pop();// check if a[i] is greater then top ele if yes then their next greater ele is a[i], therefore pop.
+                                                        // here the st will be empty or top ele is graeter than a[i]. 
+        if(st.empty()) ans.push_back(-1);// when st is empty pushback -1 to ans 
+        else ans.push_back(st.top()); // if not empty then the top ele is greater than a[i], pushback top to ans.
 
-        st.push(a[i]);
+        st.push(a[i]); // push a[i] to stack.
     }
     reverse(ans.begin(),ans.end());
     for(auto it : ans){
@@ -23,3 +26,16 @@ int main(){
     int a[n] = {4,5,2,10,8};
     nextGreaterEle(a,n);
 }
+
+/*
+next greater ele in a circular arr means after the n-1 index is 0th index.
+a[] = {2,10,12,1,11} = ans = 10 12 -1 11 12
+
+logic -> for(int i = 2*n-1; i >= 0; i--){
+        while(!st.empty() && st.top() <= a[i%n]) st.pop(); 
+
+        if(i < n) ans.push_back((st.empty()) ? -1 : st.top());
+
+        st.push(a[i%n]); 
+    }
+*/
